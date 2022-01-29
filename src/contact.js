80e08d81b82622df9949b893a66947d12e7d1d75
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-29 12:40:13 -0800

    amlostCompleted Contacts page

--- src/contact.js
+++ src/contact.js
@@ -2,16 +2,29 @@ function Contact() {
     return (
         <div className="App">
             <div>
-        <h2>Join us on our Mission to diagnose Melanoma effectilvely</h2>
-        <h3>Keep in touch</h3>
-                <img src="https://www.bing.com/images/search?view=detailV2&ccid=xSz9qY7M&id=8263EB58A454D80458D0F8BCE5A02783D3744015&thid=OIP.xSz9qY7MYSVCdQGh7YixoAHaEp&mediaurl=https%3a%2f%2fwww.lyfemarketing.com%2fwp-content%2fuploads%2f2013%2f06%2fpeople-happy-cheering.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.c52cfda98ecc6125427501a1ed88b1a0%3frik%3dFUB004MnoOW8%252bA%26pid%3dImgRaw%26r%3d0&exph=370&expw=590&q=happy+person&simid=608051980734701393&FORM=IRPRST&ck=EFC0AFF399516C6499F222B6D546E9E2&selectedIndex=155" style={{height:"50%"}}></img>
-                <div style={{width:"80%", backgroundColor:"#d9acc8", fontSize:"50px", textAlign:"center"}}>Find out what we are doing now.</div><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{width:"20%"}}/>
-                <img src='https://library.kissclipart.com/20190201/clw/kissclipart-youtube-logo-play-clipart-youtube-play-buttons-com-e62b551e3cb6ddbb.png' style={{width:"20%"}}/>Watch our videos<div style={{width:"80%", backgroundColor:"#c49670", fontSize:"50px", textAlign:"center"}}></div>
-                <div style={{width:"80%", backgroundColor:"#d9acc8", fontSize:"50px", textAlign:"center"}}>Talk to us on twitter</div><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{width:"20%"}}/>
-                <img src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{width:"20%"}}/>Follow us on facebook<div style={{width:"80%", backgroundColor:"#c49670", fontSize:"50px", textAlign:"center"}}></div>
+                <h1>Join us on our Mission to diagnose Melanoma effectilvely</h1>
+                <table style={{marginLeft:"auto", marginRight:"auto"}}>
+                    <tr>
+                        <td><div style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center"}}>  Recieve constant updates.  </div></td>
+                        <td><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{height:"80px"}}/></td>
+                        
+                    </tr>
+                    <tr>     
+                        <td><div style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center"}}>  Watch our videos  </div></td>
+                        <td><img src='https://www.seeklogo.net/wp-content/uploads/2016/06/YouTube-icon-big.png' style={{height:"50px"}}/></td>
+                    </tr>
+                    <tr>
+                        <td><td><div style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center"}}>  Talk to us on Twitter   </div></td></td>
+                        <td><td><img src='https://clipartcraft.com/images/twitter-logo-transparent-background-2.png' style={{height:"60px"}}/></td></td>
+                    </tr>
+                    <tr> 
+                        <td><div style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center"}}>  Follow us on Facebook  </div></td>
+                        <td><img src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{height:"80px"}}/></td> 
+                    </tr>
+               </table>
             </div>
         </div>
     );
 }
 
-export default Contact;
+export default Contact
