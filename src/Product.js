84d0890179e18c738fbbf456d43b19eed57689eb
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-29 11:02:52 -0800

    teachable machine

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import Navbar from './navbar.js';
+import Footer from './footer.js'
+import './style.css';
+function Product(){
+    return (
+        <div>
+            <h2>dont belive us? try our product out for yourself</h2>
+            <button onclick="window.location.href='https://teachablemachine.withgoogle.com/models/dHWSXLxB1/" style={{fontSize: "50px", backgroundColor: "#008CBA", width: "50%"}}/>
+        </div>
+    );
+}
+
+export default Product;
--- src/blog.js
+++ src/blog.js
@@ -1,9 +1,13 @@
+import Navbar from './navbar.js';
+import Footer from './footer.js'
+import './style.css';
 function Blog(){
     return (
         <div>
             <h2>Join us on our Mission to diagnose Melanoma effectilvely</h2>
             <h3>Keep up with the most recent updates</h3>
             <img src="https://www.lyfemarketing.com/wp-content/uploads/2013/06/people-happy-cheering.jpg" height="50%"></img>
+            
         </div>
     );
 }
