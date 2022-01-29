61ffaf45c1fc97c883052f1cd9111b2a2fb2871c
ChiChi <CheeChizzle@users.noreply.github.com>
2022-01-29 16:53:18 -0500

    updated socials

--- src/footer.js
+++ src/footer.js
@@ -7,9 +7,9 @@ function Footer() {
                     </div> */}
                     <div className="footerbuttons">
                         {/* w3schools stuff */}
-                        <a href="https://www.youtube.com/channel/UCSSbZRKHS72VuwhjX0-APKg" className="fa fa-youtube" alt="YouTube" target="_blank" rel="noreferrer">&#8205;</a>
-                        <a href="https://www.instagram.com/rwc_2021/" className="fa fa-instagram" alt="Instagram" target="_blank" rel="noreferrer">&#8205;</a>
-                        <a href="https://twitter.com/FacultyRwc" className="fa fa-twitter" alt="Twitter" target="_blank" rel="noreferrer">&#8205;</a>
+                        <a href="https://www.youtube.com/channel/UCpeWHQ0cHxD5KP7pPy07D8Q" className="fa fa-youtube" alt="YouTube" target="_blank" rel="noreferrer">&#8205;</a>
+                        <a href="https://www.instagram.com/firstlookdermatologic/" className="fa fa-instagram" alt="Instagram" target="_blank" rel="noreferrer">&#8205;</a>
+                        <a href="https://www.twitter.com/FirstLookDerma" className="fa fa-twitter" alt="Twitter" target="_blank" rel="noreferrer">&#8205;</a>
                         <a href="https://www.facebook.com/FirstLook-112298388026866/" className="fa fa-facebook" alt="Facebook" target="_blank" rel="noreferrer">&#8205;</a>
                     </div>
                     <div className="copyright">
