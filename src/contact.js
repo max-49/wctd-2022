22fda672ab9917952b975bfcee4912eacff6cf78
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-29 13:10:42 -0800

    The contacts page done

--- src/contact.js
+++ src/contact.js
@@ -6,20 +6,20 @@ function Contact() {
                 <table style={{marginLeft:"auto", marginRight:"auto"}}>
                     <tr>
                         <td><div style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center"}}>  Recieve constant updates.  </div></td>
-                        <td><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{height:"80px"}}/></td>
+                        <td><a href="https://www.instagram.com/firstlookdermatologic/"><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{height:"80px"}}/></a></td>
                         
                     </tr>
                     <tr>     
                         <td><div style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center"}}>  Watch our videos  </div></td>
-                        <td><img src='https://www.seeklogo.net/wp-content/uploads/2016/06/YouTube-icon-big.png' style={{height:"50px"}}/></td>
+                        <td><a href="https://www.youtube.com/channel/UCpeWHQ0cHxD5KP7pPy07D8Q"><img src='https://www.seeklogo.net/wp-content/uploads/2016/06/YouTube-icon-big.png' style={{height:"50px"}}/></a></td>
                     </tr>
                     <tr>
-                        <td><td><div style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center"}}>  Talk to us on Twitter   </div></td></td>
-                        <td><td><img src='https://clipartcraft.com/images/twitter-logo-transparent-background-2.png' style={{height:"60px"}}/></td></td>
+                        <td><div style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center"}}>  Talk to us on Twitter   </div></td>
+                        <td><a href="https://twitter.com/FirstLookDerma"><img src='https://clipartcraft.com/images/twitter-logo-transparent-background-2.png' style={{height:"60px"}}/></a></td>
                     </tr>
                     <tr> 
                         <td><div style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center"}}>  Follow us on Facebook  </div></td>
-                        <td><img src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{height:"80px"}}/></td> 
+                        <td><a href="https://www.facebook.com/FirstLook-112298388026866/"><img src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{height:"80px"}}/></a></td> 
                     </tr>
                </table>
             </div>
