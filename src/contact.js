2aa9446f0db73fe38b3159a1e0df9e8ed18a426d
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-29 11:56:37 -0800

    Contacts access commit

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,15 @@
-function Contact() {
-    return (
-        <div className="App">
-            <h1>don't talk to us</h1>
-        </div>
-    );
+import logo from './logo.svg';
+import Navbar from './Navbar';
+import './style.css';
+function Contact(){
+    <div>
+        <h2>Join us on our Mission to diagnose Melanoma effectilvely</h2>
+        <h3>Keep in touch</h3>
+        <img src="https://www.bing.com/images/search?view=detailV2&ccid=xSz9qY7M&id=8263EB58A454D80458D0F8BCE5A02783D3744015&thid=OIP.xSz9qY7MYSVCdQGh7YixoAHaEp&mediaurl=https%3a%2f%2fwww.lyfemarketing.com%2fwp-content%2fuploads%2f2013%2f06%2fpeople-happy-cheering.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.c52cfda98ecc6125427501a1ed88b1a0%3frik%3dFUB004MnoOW8%252bA%26pid%3dImgRaw%26r%3d0&exph=370&expw=590&q=happy+person&simid=608051980734701393&FORM=IRPRST&ck=EFC0AFF399516C6499F222B6D546E9E2&selectedIndex=155" style={{height:"50%"}}></img>
+        <div style={{width:"80%", backgroundColor:"#d9acc8", fontSize:"50px", textAlign:"center"}}>Find out what we are doing now.</div><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{width:"20%"}}/>
+        <img src='https://library.kissclipart.com/20190201/clw/kissclipart-youtube-logo-play-clipart-youtube-play-buttons-com-e62b551e3cb6ddbb.png' style={{width:"20%"}}/>Watch our videos<div style={{width:"80%", backgroundColor:"#c49670", fontSize:"50px", textAlign:"center"}}></div>
+        <div style={{width:"80%", backgroundColor:"#d9acc8", fontSize:"50px", textAlign:"center"}}>Talk to us on twitter</div><img src='https://openasset.com/wp-content/uploads/2018/12/instagram-logo-png-transparent.png' style={{width:"20%"}}/>
+        <img src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{width:"20%"}}/>Follow us on facebook<div style={{width:"80%", backgroundColor:"#c49670", fontSize:"50px", textAlign:"center"}}></div>
+   </div>
 }
-
 export default Contact;
