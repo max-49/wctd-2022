c32868d27a2e616ca39fa143bbc4933bcb39b664
max49 <massimomarino49@gmail.com>
2022-01-29 15:41:32 -0500

    commiting changes bc

--- src/navbar.js
+++ src/navbar.js
@@ -5,15 +5,15 @@ import { Routes, Route, Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{backgroundColor: "#B59286"}}>
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <Link className="navbar-brand" style={{height: "100%"}} to="/">
-                        <img src={Logo} alt="our logo" height="45px" />
+                    <Link style={{height: "100%", paddingTop: "5px"}} to="/">
+                        <img src={Logo} alt="our logo" height="15px" />
                     </Link>
                     <li className="nav-item">
                     <Link className="nav-link active" aria-current="page" to="/">Home</Link>
@@ -42,10 +42,6 @@ function Navbar() {
                     <Link className="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
                     </li> */}
                 </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
             </nav>
