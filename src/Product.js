ff3d40aa4d356c998ae01145ffcc7bc1b8ac67a4
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-30 01:54:31 -0800

    styling changes

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,10 @@
 import './style.css';
 function Product(){
     return (
-        <div style={{textAlign:"center"}}>
+        <div style={{textAlign:"center", backgroundColor:"#f2e9ef", textAlign:"center", padding:"10px"}}>
             <h1>Try our model now for FREE</h1>
             <h2>Take the First Look</h2>
-            <a href="https://teachablemachine.withgoogle.com/models/bQ47kn-g1/"><div style={{width:"100%", border:"double", fontSize:"30px", color:"black"}}><img alt='' src="https://i.pinimg.com/originals/92/7a/9a/927a9a70139a4e3a876b622ccb1f1b49.jpg" style={{width:"80%"}}/><br/>Use our Model<br/><br/></div></a>
+            <div style={{width:"100%", border:"double", fontSize:"30px", color:"black"}}><img alt='' src="https://i.pinimg.com/originals/92/7a/9a/927a9a70139a4e3a876b622ccb1f1b49.jpg" style={{width:"80%"}}/><br/><a class='li' href="https://teachablemachine.withgoogle.com/models/bQ47kn-g1/"><div style={{border:"solid", backgroundColor:"#f5d7ea"}}>Click Here to Try our Model</div></a><br/><br/></div>
         </div>
     );
 }
--- src/contact.js
+++ src/contact.js
@@ -1,23 +1,24 @@
 function Contact() {
     return (
         <div className="App">
-            <div style= {{borderStyle:"double", borderWidth:"8px", backgroundColor:"#c49680", textAlign:"center", padding:"10px"}}>
-                <h1 style={{textAlign:"center"}}>Join us on our Mission to diagnose Melanoma effectilvely</h1>
+            <div style= {{borderStyle:"outset", borderWidth:"8px", backgroundColor:"#f2e9ef", textAlign:"center", padding:"10px"}}>
+                <h1 style={{textAlign:"center"}}>Join us on our Mission to Diagnose Melanoma Effectively</h1>
+                <br/><br/>
                 <table style={{marginLeft:"auto", marginRight:"auto"}}>
-                    <tr style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center", borderStyle:"double", borderWidth:"8px", padding:"10px"}}>
-                        <td>Recieve constant updates.</td>
+                    <tr style={{width:"100%", backgroundColor:"#f9e1f1", fontSize:"30px", textAlign:"center", borderStyle:"double", borderWidth:"8px", padding:"10px"}}>
+                        <td>Receive constant updates.</td>
                         <td><a href="https://www.instagram.com/firstlookdermatologic/"><img alt='' src='https://clipartcraft.com/images/instagram-logo-transparent-png-format-3.png' style={{height:"80px"}}/></a></td>
                         
                     </tr>
-                    <tr style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}>     
+                    <tr style={{width:"100%", backgroundColor:"#faeee1", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}>     
                         <td> Watch our videos  </td>
                         <td><a href="https://www.youtube.com/channel/UCpeWHQ0cHxD5KP7pPy07D8Q"><img alt='' src='https://all-4-one.com/wp-content/uploads/2020/12/youtube-icon-logo-logo-icon-png-svg.png' style={{height:"50px"}}/></a></td>
                     </tr>
-                    <tr style={{width:"100%", backgroundColor:"#d9acc8", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}> 
+                    <tr style={{width:"100%", backgroundColor:"#f9e1f1", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}> 
                         <td>  Talk to us on Twitter   </td>
                         <td><a href="https://twitter.com/FirstLookDerma"><img alt='' src='https://clipartcraft.com/images/twitter-logo-transparent-background-2.png' style={{height:"50px"}}/></a></td>
                     </tr>
-                    <tr style={{width:"100%", backgroundColor:"#c49670", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}> 
+                    <tr style={{width:"100%", backgroundColor:"#faeee1", fontSize:"30px", textAlign:"center" , borderStyle:"double", borderWidth:"8px", padding:"10px"}}> 
                         <td>  Follow us on Facebook  </td>
                         <td><a href="https://www.facebook.com/FirstLook-112298388026866/"><img alt='' src='https://www.freeiconspng.com/uploads/facebook-logo-facebook-logo-9.png' style={{height:"80px"}}/></a></td> 
                     </tr>
