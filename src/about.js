101ee46fa37dd592e4424807c68dd0bfe4c26c27
ChiChi <CheeChizzle@users.noreply.github.com>
2022-01-30 09:58:43 -0500

    fixed about page

--- src/about.js
+++ src/about.js
@@ -9,27 +9,28 @@ function About() {
                 <div class="container">
                     <div class="intro">
                         <h2 class="text-center">Who We Are </h2>
+                        <p class="text-center">Meet the team of innovators who made this revolutionary product a reality.</p>
                     </div>
                     <div class="row people">
                         <div class="col-md-6 col-lg-4 item">
                             <div class="box"><img src={anika} class="rounded-circle" alt='' id = "anika"/>
                                 <h3 class="name">Anika Mahesh</h3>
-                                <p class="title">Musician</p>
-                                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                                <p class="title">Willows Preparatory School</p>
+                                <p class="description">Anika Mahesh is a sophomore at Willows Preparatory School. As our website programmer and media specialist, she built our blog page, managed our social presence, and led the process of designing our machine learning model. </p>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4 item">
                             <div class="box"><img src={chichi} class="rounded-circle" alt='' id = "chichi" />
                                 <h3 class="name">Chigozirim Ifebi</h3>
-                                <p class="title">Artist</p>
-                                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                                <p class="title">Elmont Memorial High School</p>
+                                <p class="description">Chigozirim is a junior at Elmont Memorial High School. She partook in programming the webpage, more specifically the "About us" page and the footer design. She also assisted with the company's graphic design as the illustrator of the company's logo and app interface. </p>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4 item">
                             <div class="box"><img src={max} class="rounded-circle" alt='' />
                                 <h3 class="name">Massimo Marino</h3>
-                                <p class="title">Stylist</p>
-                                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                                <p class="title">John F. Kennedy High School</p>
+                                <p class="description">Massimo is a junior at John F. Kennedy High School. His main role in this project was bringing all parts of the website together into one website and managing design across the entire website. He led the process of designing the front page and making other look uniform.</p>
                             </div>
                         </div>
                     </div>
