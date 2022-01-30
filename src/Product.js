2a4b65d3f8753999d2473b727f7974c305dca519
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-29 19:01:15 -0800

    commit

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,10 @@
 import './style.css';
 function Product(){
     return (
-        <div>
-            <h2>dont belive us? try our product out for yourself</h2>
-            <button onclick="window.location.href='https://teachablemachine.withgoogle.com/models/dHWSXLxB1/" style={{fontSize: "50px", backgroundColor: "#008CBA", width: "50%"}}/>
+        <div style={{textAlign:"center"}}>
+            <h1>Try our model now for FREE</h1>
+            <h2>Take the First Look</h2>
+            <a href="https://teachablemachine.withgoogle.com/models/bQ47kn-g1/"><div style={{width:"100%", border:"double", fontSize:"30px", color:"black"}}><img src="https://i.pinimg.com/originals/92/7a/9a/927a9a70139a4e3a876b622ccb1f1b49.jpg" style={{width:"80%"}}/><br/>Use our Model<br/><br/></div></a>
         </div>
     );
 }
