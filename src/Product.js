ad0595fc0aa0d11a545dca4c408663e3d4eaa61b
max49 <massimomarino49@gmail.com>
2022-01-30 09:36:20 -0500

    fix dupes

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,7 @@
 import './style.css';
 function Product(){
     return (
-        <div style={{textAlign:"center", backgroundColor:"#f2e9ef", textAlign:"center", padding:"10px"}}>
+        <div style={{textAlign:"center", backgroundColor:"#f2e9ef", padding:"10px"}}>
             <h1>Try our model now for FREE</h1>
             <h2>Take the First Look</h2>
             <div style={{width:"100%", border:"double", fontSize:"30px", color:"black"}}><img alt='' src="https://i.pinimg.com/originals/92/7a/9a/927a9a70139a4e3a876b622ccb1f1b49.jpg" style={{width:"80%"}}/><br/><a class='li' href="https://teachablemachine.withgoogle.com/models/bQ47kn-g1/"><div style={{border:"solid", backgroundColor:"#f5d7ea"}}>Click Here to Try our Model</div></a><br/><br/></div>
